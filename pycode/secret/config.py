b8bac4a1c17dac6d2fc623ad92d50c532d57da0a
leafnull <2281314960@qq.com>
2019-04-21 17:22:06 +0800

    third version

--- pycode/secret/config.py
+++
@@ -1,8 +0,0 @@
-# this config is secret and should never be exposed
-class Dev_config():
-    PASSWORD = '12345'
-    TOKEN = 'alsk1029DFKJ557'
-
-class Test_config():
-    PASSWORD = 'formyLOVE'
-    TOKEN = 'alsk1029DFKJ557'
